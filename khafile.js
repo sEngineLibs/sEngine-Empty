62c0ba0277ac8173c169650e65d5f5ff7b6e97ed
smoothie <kibot.email@gmail.com>
2025-02-08 23:45:42 +0800

    Update project

--- khafile.js
+++ khafile.js
@@ -6,6 +6,10 @@ project.addAssets("assets/**", {
     destination: "assets/{dir}/{name}",
     name: "{dir}/{name}",
 });
+process.defines = [
+    "S2D_DEBUG_FPS",
+    "S2D_UI_DEBUG_ELEMENT_BOUNDS"
+];
 await project.addProject("sEngine");
 
 resolve(project);
--- src/Main.hx
+++ src/Main.hx
@@ -1,26 +1,38 @@
 package;
 
-import se.Timer;
 import se.system.Application;
 import se.ui.elements.TextElement;
 
 class Main {
 	public static function main() {
+		// start the application
 		Application.start({
-			title: "Untitled",
+			title: "untitled",
 			width: 1024,
 			height: 1024
-		}, function() {
-			var text = new TextElement("Hello, sEngine!");
-			text.color = "red";
-			text.fontSize = 72;
-			text.alignment = Center;
-			text.width = 1024;
-			text.height = 1024;
+		}, init); // a function that is executed after the application is initialized
+	}
 
-			new Timer(() -> {
-				text.color.h = 0.01;
-			}, 1 / 60).repeat(0);
+	static function init() {
+		var scene = se.ui.UIScene.current; // UIScene is a root container for ui elements
+		scene.padding = 128;
+
+		var text = new TextElement("Hello, sEngine!", scene);
+		text.padding = 128;
+		text.anchors.margins = 128;
+		text.anchors.fill(scene);
+		text.color = "red";
+		text.fontSize = 72;
+		text.alignment = Center;
+
+		// change the hue of the text color 60 times per second
+		new se.Timer(() -> {
+			text.color.h += 0.015;
+		}, 1 / 60).repeat(0);
+
+		// change the font size based on the mouse wheel delta
+		Application.input.mouse.notifyOnScrolled((delta) -> {
+			text.fontSize += delta;
 		});
 	}
 }

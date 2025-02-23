e888b24f693f9d2a525eaf7fd45868177b6b7a72
smoothie-ws <kibot.email@gmail.com>
2025-02-23 18:41:00 +1000

    Update example

--- src/Main.hx
+++ src/Main.hx
@@ -1,18 +1,14 @@
 package;
 
-import se.system.Application;
-
 class Main {
 	public static function main() {
 		// start the application
-		Application.start({
+		se.Application.start({
 			title: "untitled",
 			width: 1024,
 			height: 1024
 		}, init); // a function that is executed after the application is initialized
 	}
 
-	static function init() {
-		
-	}
+	static function init() {}
 }

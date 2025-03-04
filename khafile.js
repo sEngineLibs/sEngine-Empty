616126d60e5a79ee59d20152adbaac0aed472773
smoothie-ws <kibot.email@gmail.com>
2025-03-04 22:12:23 +1000

    Update `sEngine`

--- khafile.js
+++ khafile.js
@@ -8,7 +8,7 @@ project.addAssets("assets/**", {
 });
 process.defines = [
     "S2D_DEBUG_FPS",
-    "S2D_UI_DEBUG_ELEMENT_BOUNDS"
+    // "S2D_UI_DEBUG_ELEMENT_BOUNDS"
 ];
 await project.addProject("sEngine");
 
--- src/Main.hx
+++ src/Main.hx
@@ -3,12 +3,17 @@ package;
 class Main {
 	public static function main() {
 		// start the application
-		se.Application.start({
+		se.App.start({
 			title: "untitled",
 			width: 1024,
-			height: 1024
+			height: 1024,
+			framebuffer: {
+				samplesPerPixel: 2
+			}
 		}, init); // a function that is executed after the application is initialized
 	}
 
-	static function init() {}
+	static function init() {
+
+	}
 }

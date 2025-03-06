105f3f606fdee40c8f854572fcd4f02aea405940
smoothie-ws <kibot.email@gmail.com>
2025-03-06 15:39:26 +1000

    Update `sEngine`

--- src/Main.hx
+++ src/Main.hx
@@ -6,14 +6,11 @@ class Main {
 		se.App.start({
 			title: "untitled",
 			width: 1024,
-			height: 1024,
-			framebuffer: {
-				samplesPerPixel: 2
-			}
+			height: 1024
 		}, init); // a function that is executed after the application is initialized
 	}
 
 	static function init() {
-
+		
 	}
 }
